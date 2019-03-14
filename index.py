68eeb50ee8f79269e0a70c1c7605dc3ca103001f
Dmitry <dzmitry.dulevich@itechart-group.com>
2019-03-14 18:15:19 +0300

    Empty

--- index.py
+++ index.py
@@ -12,8 +12,7 @@ bot = SpeechToTextBot(bot_config)
 loop = asyncio.get_event_loop()
 loop.run_until_complete(bot.start_listening())
 
-try:
-    print(os.environ)
-except:
-    pass
-socket = socket.socket().bind('', os.environ["PORT"]) if "PORT" in os.environ else None
+print(os.environ)
+print('App starting')
+
+socket = socket.socket().bind('0.0.0.0', os.environ["PORT"]) if "PORT" in os.environ else None
