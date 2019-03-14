17aca5d03083be31d3c216bf29d6b460bceb9967
Dmitry <dzmitry.dulevich@itechart-group.com>
2019-03-14 17:17:00 +0300

    Added mpeg for ubuntu

--- modules/convert_ogg_module.py
+++ modules/convert_ogg_module.py
@@ -9,11 +9,14 @@ from helpers import asyncronize_function
 st = os.stat("./ffmpeg.exe")
 os.chmod("./ffmpeg.exe", st.st_mode | stat.S_IEXEC)
 
+st = os.stat("./ffmpeg")
+os.chmod("./ffmpeg", st.st_mode | stat.S_IEXEC)
+
 async def convert(voice_ogg_content):
     loop = asyncio.get_event_loop()
     completedProcess = await asyncronize_function(
         subprocess.run,
-        ["./ffmpeg.exe", "-i", "pipe:0", "-f", "wav", "pipe:1"],
+        ["./ffmpeg", "-i", "pipe:0", "-f", "wav", "pipe:1"],
         input=voice_ogg_content,
         capture_output=True
     )
