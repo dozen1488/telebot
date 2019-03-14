04a44a24709a23d2e6127df5e2803c1c235cae92
Dmitry <dzmitry.dulevich@itechart-group.com>
2019-03-14 18:23:26 +0300

    Empty

--- index.py
+++ index.py
@@ -1,8 +1,14 @@
 import asyncio
 import os
 import socket 
+import threading
 
 from modules.speech_to_text_bot import *
+
+print(os.environ)
+print('App starting')
+
+socket = socket.socket().bind('0.0.0.0', os.environ["PORT"]) if "PORT" in os.environ else None
 
 bot_config = {
     "api_token": os.environ["TELRGRAM_API_TOKEN"]
@@ -11,8 +17,3 @@ bot = SpeechToTextBot(bot_config)
 
 loop = asyncio.get_event_loop()
 loop.run_until_complete(bot.start_listening())
-
-print(os.environ)
-print('App starting')
-
-socket = socket.socket().bind('0.0.0.0', os.environ["PORT"]) if "PORT" in os.environ else None
