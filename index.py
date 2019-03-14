a40033e6101af5c619e5b3376334422f93b14de8
Dmitry <dzmitry.dulevich@itechart-group.com>
2019-03-14 17:56:27 +0300

    Added port listening

--- index.py
+++ index.py
@@ -1,5 +1,6 @@
 import asyncio
 import os
+import socket 
 
 from modules.speech_to_text_bot import *
 
@@ -10,3 +11,5 @@ bot = SpeechToTextBot(bot_config)
 
 loop = asyncio.get_event_loop()
 loop.run_until_complete(bot.start_listening())
+
+socket = socket.socket().bind('', os.environ["PORT"]).listen(1) if "PORT" in os.environ
